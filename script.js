a6c073622c4b211ef33026b03f8cd19502a14361
Mario <132083365+mariotrg@users.noreply.github.com>
2023-05-05 20:35:53 +0100

    Basic calc functions added

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+let firstOperand = 0;
+let secondOperand = 0;
+let operator;
 
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function substract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function operate(operator) {
+  switch (operator) {
+    case "+":
+      return add(firstOperand, secondOperand);
+    case "-":
+      return substract(firstOperand, secondOperand);
+    case "*":
+      return multiply(firstOperand, secondOperand);
+    case "/":
+      return divide(firstOperand, secondOperand);
+  }
+}
