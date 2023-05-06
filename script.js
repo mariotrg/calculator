03858543f00db1f1012d811ba0ef80ec43ee0f56
Mario <132083365+mariotrg@users.noreply.github.com>
2023-05-06 13:54:41 +0100

    Added update display function, added event listener for number & operator buttons

--- script.js
+++ script.js
@@ -1,6 +1,72 @@
-let firstOperand = 0;
-let secondOperand = 0;
+let firstOperand = "";
+let secondOperand = "";
 let operator;
+let result;
+let current;
+
+const numberBtn = document.querySelectorAll(".number-btn");
+const operatorBtn = document.querySelectorAll(".operator-btn");
+const equalsBtn = document.querySelector(".equals-btn");
+const upperDisplay = document.querySelector(".upper-display");
+const lowerDisplay = document.querySelector(".lower-display");
+
+for (const button of numberBtn) {
+  button.addEventListener("click", (e) => {
+    appendNumber(e);
+    updateDisplay();
+  });
+}
+
+for (const button of operatorBtn) {
+  button.addEventListener("click", (e) => {
+    if (firstOperand && secondOperand && operator) {
+      result =
+        Math.round(
+          operate(operator, Number(firstOperand), Number(secondOperand)) * 100
+        ) / 100;
+      firstOperand = result;
+      result = "";
+      secondOperand = "";
+      updateDisplay();
+    }
+    operator = e.target.value;
+    if (result) {
+      firstOperand = result;
+      result = "";
+      secondOperand = "";
+      updateDisplay();
+    }
+    updateDisplay();
+  });
+}
+
+equalsBtn.addEventListener("click", (e) => {
+  if (!operator) return false;
+  if (firstOperand === "") firstOperand = 0;
+  if (secondOperand === "") secondOperand = 0;
+  result =
+    Math.round(
+      operate(operator, Number(firstOperand), Number(secondOperand)) * 100
+    ) / 100;
+  updateDisplay();
+});
+
+function appendNumber(e) {
+  if (!operator) firstOperand += e.target.value;
+  if (operator) secondOperand += e.target.value;
+}
+
+function updateDisplay() {
+  if (!operator) lowerDisplay.textContent = `${firstOperand}`;
+  if (operator) {
+    upperDisplay.textContent = `${firstOperand} ${operator}`;
+    lowerDisplay.textContent = `${secondOperand}`;
+  }
+  if (result) {
+    upperDisplay.textContent = `${firstOperand} ${operator} ${secondOperand}`;
+    lowerDisplay.textContent = `${result}`;
+  }
+}
 
 function add(num1, num2) {
   return num1 + num2;
@@ -18,7 +84,7 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
-function operate(operator) {
+function operate(operator, firstOperand, secondOperand) {
   switch (operator) {
     case "+":
       return add(firstOperand, secondOperand);
